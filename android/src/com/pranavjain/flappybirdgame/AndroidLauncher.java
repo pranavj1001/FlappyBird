405dd5fa6884576b9381cd561e1238ee0b14e73d
pranavj1001 <pranavj1001@gmail.com>
2016-11-07 12:54:23 +0530

    initial commit

---
+++ android/src/com/pranavjain/flappybirdgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pranavjain.flappybirdgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pranavjain.flappybirdgame.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
