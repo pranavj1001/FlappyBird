040c2c4022263e9a2a46f7a609ce8a6ab3e965d1
pranavj1001 <pranavj1001@gmail.com>
2016-11-09 01:44:11 +0530

    added the flapping motion of the bird

--- core/src/com/pranavjain/flappybirdgame/FlappyBird.java
+++ core/src/com/pranavjain/flappybirdgame/FlappyBird.java
@@ -9,20 +9,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
-    Texture bird;
+
+    Texture[] birds;
+    int flapState = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
-        bird = new Texture("bird.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
 	}
 
 	@Override
 	public void render () {
+        if(flapState == 0){
+            flapState = 1;
+        }else {
+            flapState = 0;
+        }
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        batch.draw(bird,Gdx.graphics.getWidth()/2 - bird.getWidth()/2,Gdx.graphics.getHeight()/2 - bird.getHeight()/2);
+        batch.draw(birds[flapState],Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2,Gdx.graphics.getHeight()/2 - birds[flapState].getHeight()/2);
 		batch.end();
 	}
 	
